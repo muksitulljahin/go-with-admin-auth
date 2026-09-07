df582f49141551f9e5989d19c907e1c220310800
muksituljahin <muksitul.inno@pakizasoftware.com>
2026-09-07 10:53:35 +0600

    feat: integrate gin-swagger for API documentation and UI generation

--- main.go
+++ main.go
@@ -5,7 +5,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	// docs ফোল্ডার জেনারেট হওয়ার পর এটি লাগবে
 )
+
+// @title           Go Admin & Auth API
+// @version         1.0
+// @description     This is a sample authentication server.
+// @host            localhost:8080
+// @BasePath        /api/v1
 
 const (
 	apiVersion1 = "/api/v1"
@@ -14,6 +23,10 @@ const (
 
 func main() {
 	r := gin.Default()
+
+	// Swagger UI Route
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	r.GET("ping", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "api work successfull",
@@ -21,7 +34,6 @@ func main() {
 	})
 
 	api := r.Group(apiVersion1)
-
 	auth.RegisterRoutes(api)
 
 	r.Run(port)
